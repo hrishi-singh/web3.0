08b732f31a666a24a34722462a11f84ab4d3a378
hrishi-singh <singh.123@iitj.ac.in>
2023-12-19 21:57:18 +0530

    ETH Converter Added

--- client/src/components/Exchange.jsx
+++ client/src/components/Exchange.jsx
@@ -1,57 +1,98 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Loader from "./Loader";
-import { useContext } from "react";
+import Select from 'react-select';
 import ETH_img from '../assets/eth-unscreen.gif'
-import { TransactionContext } from "../context/TransactionContext";
-const Input = ({ placeholder, name, type, value, handleChange }) => (
+
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor:'transparent', // Customize control background color
+    borderColor: '#555' , // Customize control border color
+    boxShadow: state.isFocused ? '0 0 0 1px #555' : 'none', // Customize control box shadow
+    color: 'red', 
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected ? '#555' : state.isFocused ? '#666' : '#333', // Customize option background color
+    color: state.isSelected ? '#fff' : '#ccc', 
+  }),
+};
+
+
+
+const Input = ({ placeholder,type, value,handleChange}) => (
   <input
     placeholder={placeholder}
     type={type}
     step="0.0001"
     value={value}
-    onChange={(e) => handleChange(e, name)}
+    onChange={(e) =>  handleChange(e)}
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
   />
 );
-const API_URL =
-  "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=BTC,USD,EUR&api_key=6a4234186f13b2ca0bef7921b1ff54662c76f2bf64a814af19f9267bbdd48662";
-// const ServiceCard = ({title, subtitle }) => (
-//   <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
-//     <div className="ml-5 flex flex-col flex-1">
-//       <h1 className="mt-2 text-white text-lg">{title}</h1>
-//       <p className="mt-2 text-white text-sm md:w-9/199">{subtitle}</p>
-//     </div>
-//   </div>
-// );
 
+const COIN_LIST="https://min-api.cryptocompare.com/data/all/coinlist";
+
+let op="";
 const Exchange = () => {
-  const {handleChange,isLoading} = useContext(TransactionContext);
-  const [btc, setBtc] = useState([]);
-  const exch = async () => {
-    const response = await fetch(`${API_URL}`);
-    const data = await response.json();
-    console.log(data.ETH);
-    setBtc([data.ETH.BTC, data.ETH.EUR, data.ETH.USD]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [numberInput, setNumberInput] = useState(null);
+  const [result, setResult] = useState(0);
+
+  const handleInputChange = (event) => {
+    setNumberInput(event.target.value);
   };
+
+  const handleConvert = async() => {
+    if (selectedOption) {
+      const conversion =parseFloat(selectedOption.value) * parseFloat(numberInput);
+      setResult(conversion);
+    }
+  };
+
+  const [options,setOptions]=useState([{value:'',label:''}]);
+
+  const [coinsList, setCoinsList] = useState([]);
+  const coins = async () =>{
+    fetch(`${COIN_LIST}`)
+      .then(response => response.json())
+      .then(data => setCoinsList((Object.entries(data.Data)).slice(0,50)))
+      .catch(error => console.error('Error fetching data:', error));
+    }
+
+    const opt= async()=>{
+      coinsList.map((det)=>{
+          (det[1].SortOrder<10)&&(op+=`${det[1].Symbol},`)       
+      })
+
+      op=op.slice(0, -1);
+
+      const API_URL =
+      `https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=${op}&api_key=6a4234186f13b2ca0bef7921b1ff54662c76f2bf64a814af19f9267bbdd48662`;
+      let response=await fetch(API_URL);
+      let data=await response.json();
+      data=Object.entries(data.ETH)
+      data.map((pair)=>{
+        setOptions((prev)=>[...prev,{value:pair[1],label:pair[0]}])
+      })
+    }
   useEffect(() => {
-    exch();
-  }, []);
+    // exch();
+    coins();
+    opt();
+  },[]);
+  useEffect(() => {
+    opt();
+  },[coinsList]);
+  
+  console.log(options)
+
+
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col  items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <div className="flex">
-            {/* <ServiceCard
-              title="1 ETH"
-              subtitle={
-                <ul>
-                  <li className="ETH_to_BTC">BTC : {btc[0]}</li>
-                  <li className="ETH_to_USD">USD : {btc[1]}</li>
-                  <li className="ETH_to_EUR">EUR : {btc[2]}</li>
-                </ul>
-              }
-            /> */}
             <img src={ETH_img} alt="Eth" />
             <div className="flex flex-1 justify-start flex-col mf:mr-10">
               <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
@@ -59,23 +100,27 @@ const Exchange = () => {
               </h1>
               <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                 <Input placeholder="ETH Amount" name="amount"
-              type="number"
-              handleChange={handleChange} />
+              type="number" value={numberInput}
+              handleChange={handleInputChange} />
 
-                <Input placeholder="Convert to" name="addressTo" type="text" />
+                <Select styles={customStyles} className="my-2 w-full rounded-sm p-2 outline-none bg-transparent border-none white-glassmorphism" options={options} 
+                 value={selectedOption}
+                 onChange={(selected) => setSelectedOption(selected)}/>
 
                 {
-                isLoading?(
-                  <Loader/>
-                ):
-                (
+                
                 <button
                   type="button"
-                  // onClick={handleSubmit}
+                  onClick={handleConvert}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">Convert Now
                   </button>
-                )
+                
               }
+              <div>
+                <h1>
+                  Result: {result}
+                </h1>
+              </div>
               </div>
             </div>
           </div>
