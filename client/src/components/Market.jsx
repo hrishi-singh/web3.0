1596c64a81f2e63188add2daed651dfad2a50082
hrishi-singh <singh.123@iitj.ac.in>
2023-12-21 20:29:56 +0530

    cards and title alignment fixed

--- client/src/components/Market.jsx
+++ client/src/components/Market.jsx
@@ -4,16 +4,30 @@ import { BiSearchAlt } from "react-icons/bi";
 const Market = () => {
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex mf:flex-row flex-col  items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col lg:flex-col mf:mr-10 max-w-fit items-center">
-          <div className="flex-1 flex flex-col justify-start items-start">
+      <div className="flex mf:flex-col flex-col  items-start justify-between md:p-20 py-12 px-4">
+          <div className="flex w-full justify-center items-center">
 
             <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
               Ethereum <br/>  Market Statistics
             </h1>
 
           </div>
+        <div className="flex flex-1 justify-start flex-row lg:flex-row mf:mr-10 max-w-fit items-center">
           <ServiceCard
+            color="bg-[#8945F8]"
+            title="Market Cap."
+            icon={<BiSearchAlt fontSize={21} className="text-white" />}
+            subtitle="value fetched from API"
+          />
+          
+           <ServiceCard
+            color="bg-[#8945F8]"
+            title="Market Cap."
+            icon={<BiSearchAlt fontSize={21} className="text-white" />}
+            subtitle="value fetched from API"
+          />
+
+           <ServiceCard
             color="bg-[#8945F8]"
             title="Market Cap."
             icon={<BiSearchAlt fontSize={21} className="text-white" />}
